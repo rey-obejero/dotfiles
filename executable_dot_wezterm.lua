0cba68d3c76011bdb7af203d4a5bc6835ee03932
Rey Obejero <rey.obejero.mail@gmail.com>
2026-07-15 19:30:07 +0800

    feat: map `SHIFT+Enter` to send newline on WezTerm

--- executable_dot_wezterm.lua
+++ executable_dot_wezterm.lua
@@ -8,4 +8,14 @@ config.color_scheme = "Github (base16)"
 
 config.enable_kitty_keyboard = true
 
+config.keys = {
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("\x1b[13;2u"),
+	},
+}
+
+config.warn_about_missing_glyphs = false
+
 return config
